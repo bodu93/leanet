cff64a36d894e73c73c4aa4a834e1658c71e91e4
bodu93 <bodu93@outlook.com>
2018-04-16 15:14:14 +0800

    logstream: in-memory stream class

---
+++ src/leanet/logstream.cc
@@ -0,0 +1,153 @@
+#include <leanet/logstream.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include <algorithm>
+
+namespace leanet {
+
+namespace detail {
+
+template<size_t SIZE> void FixedBuffer<SIZE>::cookieStart() { }
+template<size_t SIZE> void FixedBuffer<SIZE>::cookieEnd() { }
+template class FixedBuffer<leanet::LogStream::kSmallBuffer>;
+//template class FixedBuffer<leanet::LogStream::kLargeBuffer>;
+
+const char digits[] = "9876543210123456789";
+const char* zero = digits + 9;
+const char digitsHex[] = "0123456789ABCDEF";
+
+template<typename T>
+size_t convert(char buf[], T value) {
+	T i = value;
+	char* p = buf;
+
+	do {
+		// note: lsd may be negative number,
+		// so the digits array is subtle
+		int lsd = static_cast<int>(i % 10);
+		i /= 10;
+		*p++ = zero[lsd];
+	} while (i != 0);
+
+	if (value < 0) {
+		*p++ = '-';
+	}
+	*p = '\0';
+	std::reverse(buf, p);
+	return p - buf;
+}
+
+size_t convertHex(char buf[], uintptr_t value) {
+	uintptr_t i = value;
+	char* p = buf;
+
+	do {
+		int lsd = static_cast<int>(i % 16);
+		assert(lsd >= 0);
+		i /= 16;
+		*p++ = digitsHex[lsd];
+	} while (i != 0);
+
+	*p = '\0';
+	std::reverse(buf, p);
+	return p - buf;
+}
+
+template<typename T>
+Fmt::Fmt(const char* fmt, T val) {
+	length_ = snprintf(buf_, sizeof(buf_), fmt, val);
+	assert(length_ < sizeof(buf_));
+}
+
+// explicit instantiations
+template Fmt::Fmt(const char* fmt, char);
+template Fmt::Fmt(const char* fmt, short);
+template Fmt::Fmt(const char* fmt, unsigned short);
+template Fmt::Fmt(const char* fmt, int);
+template Fmt::Fmt(const char* fmt, unsigned int);
+template Fmt::Fmt(const char* fmt, long);
+template Fmt::Fmt(const char* fmt, unsigned long);
+template Fmt::Fmt(const char* fmt, long long);
+template Fmt::Fmt(const char* fmt, unsigned long long);
+template Fmt::Fmt(const char* fmt, float);
+template Fmt::Fmt(const char* fmt, double);
+
+}
+
+template<typename T>
+void LogStream::appendInteger(T val) {
+	if (buffer_.avail() >= kMaxNumericSize) {
+		size_t len = convert(buffer_.current(), val);
+		buffer_.advance(len);
+	}
+}
+template LogStream::appendInteger(int val);
+template LogStream::appendInteger(unsigned int val);
+template LogStream::appendInteger(long val);
+template LogStream::appendInteger(unsigned long val);
+template LogStream::appendInteger(long long val);
+template LogStream::appendInteger(unsigned long long val);
+
+LogStream& LogStream::operator<<(short v) {
+	*this << static_cast<int>(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned short v) {
+	*this << static_cast<unsigned int>(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(int v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned int v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(long v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned long v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(long long v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned long long v) {
+	appendInteger(v);
+	return *this;
+}
+
+LogStream& LogStream::operator<<(const void* p) {
+	uintptr_t v = reinterpret_cast<uintptr_t>(p);
+	if (buffer_.avail() >= kMaxNumericSize) {
+		char* buf = buffer_.current();
+		buf[0] = '0';
+		buf[1] = 'x';
+		size_t len = convertHex(buf+2, v);
+		buffer_.advance(len);
+	}
+	return *this;
+}
+
+LogStream& LogStream::operator<<(double v) {
+	if (buffer_.avail() >= kMaxNumericSize) {
+		int len = snprintf(buffer_.current(), kMaxNumericSize, "%.12g", v);
+		buffer_.advance(static_cast<size_t>(len));
+	}
+	return *this;
+}
+
+}
--- src/leanet/logstream.h
+++ src/leanet/logstream.h
@@ -2,7 +2,7 @@
 #define LEANET_LOGSTREAM_H
 
 #include <assert.h>
-#include <string.h>
+#include <string.h> // bzero
 #include <string>
 
 #include <leanet/noncopyable.h>
@@ -12,7 +12,156 @@ namespace leanet {
 
 namespace detail {
 
+template<size_t SIZE>
+class FixedBuffer: noncopyable {
+private:
+	void (*cookie_)();
+	char buffer_[SIZE];
+	char* cur_;
+
+	const char* end() const {
+		return cur_ + SIZE;
+	}
+
+	static void cookieStart();
+	static void cookieEnd();
+
+public:
+	FixedBuffer()
+		: cur_(buffer_) {
+		setCookie(cookieStart);
+	}
+
+	~FixedBuffer() {
+		setCookie(cookieEnd);
+	}
+
+	void append(const char* data, size_t len) {
+		if (avail() > len) {
+			::memcpy(cur_, data, len);
+			cur_ += len;
+		}
+	}
+
+	const char* buffer() const { return buffer_; }
+	size_t length() const { return static_cast<size_t>(cur_ - buffer_); }
+
+	char* current() { return cur_; }
+	// bytes left length
+	size_t avail() const { return static_cast<size_t>(end() - cur_); }
+	void advance(size_t n) { cur_ += n; }
+
+	void rewind() { cur_ = buffer_; }
+	void bzero() { ::bzero(buffer_, SIZE); }
+
+	std::string toString() const { return std::string(buffer_, SIZE); }
+	StringView toStringView() const { return StringView(buffer_, SIZE); }
+};
+
 }
+
+class Fmt {
+public:
+	template<typename T>
+	Fmt(const char* fmt, T val);
+
+	const char* data() const { return buf_; }
+	int length() const { return length_; }
+
+private:
+	char buf_[32];
+	int length_;
+};
+
+// see cfront1.0's stream.h:
+// http://www.softwarepreservation.org/projects/c_plus_plus/cfront/release_1.0/src/cfront/incl/stream.h/view
+class LogStream: noncopyable {
+	typedef LogStream self;
+public:
+	static const size_t kSmallBufferSize = 4000;
+	static const size_t kLargeBufferSize = 4000*1000;
+	typedef detail::FixedBuffer<kSmallBufferSize> Buffer;
+
+	// integers
+	self& operator<<(bool x) {
+		const char* s = x ? "true" : "false";
+		buffer_.append(s, strlen(s));
+		return *this;
+	}
+
+	self& operator<<(short);
+	self& operator<<(unsigned short);
+	self& operator<<(int);
+	self& operator<<(unsigned int);
+	self& operator<<(long);
+	self& operator<<(unsigned long);
+	self& operator<<(long long);
+	self& operator<<(unsigned long long);
+
+	self& operator<<(const void*); // variant types
+
+	// float-point numbers
+	self& operator<<(float f) {
+		return operator<<(static_cast<double>(f));
+	}
+
+	self& operator<<(double);
+
+	// character-based variables
+	self& operator<<(char c) {
+		buffer_.append(&c, 1);
+		return *this;
+	}
+
+	// c-style string
+	self& operator<<(const char* str) {
+		if (str) {
+			buffer_.append(str, strlen(str));
+		} else {
+			buffer_.append("(null)", 6);
+		}
+		return *this;
+	}
+
+	// binary buffer
+	self& operator<<(const unsigned char* str) {
+		return operator<<(reinterpret_cast<const char*>(str));
+	}
+
+	self& operator<<(const std::string& v) {
+		buffer_.append(v.c_str(), v.size());
+		return *this;
+	}
+
+	self& operator<<(const StringView& v) {
+		buffer_.append(v.data(), v.length());
+		return *this;
+	}
+
+	self& operator<<(const Buffer& v) {
+		return operator<<(v.toStringView());
+	}
+
+	self& operator<<(const Fmt& fmt) {
+		buffer_.append(fmt.data(), fmt.length());
+		return *this;
+	}
+
+	void append(const char* data, int len) {
+		buffer_.append(data, len);
+	}
+
+	const Buffer& buffer() const { return buffer_; }
+	void resetBuffer() { buffer_.rewind(); }
+
+private:
+	static const size_t kMaxNumericSize = 32;
+
+	template<typename T>
+	void appendInteger(T val);
+
+	Buffer buffer_;
+};
 
 }
 
